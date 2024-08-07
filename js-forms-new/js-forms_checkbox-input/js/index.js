4bde66c5a9d602fda6789445e79fbe84e165c19d
AntoineFachez <anthony.zornig@gmx.de>
2024-08-07 16:10:51 +0200

    js-forms_checkbox-input

--- js-forms-new/js-forms_checkbox-input/js/index.js
+++ js-forms-new/js-forms_checkbox-input/js/index.js
@@ -4,18 +4,31 @@ const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
 
-function hideTosError() {
-  tosError.setAttribute("hidden", "");
+const complaintSuccessfull = document.querySelector('[data-js="success"]');
+
+function hideTosError(el) {
+  el.setAttribute("hidden", "");
 }
 
-function showTosError() {
-  tosError.removeAttribute("hidden");
+function showTosError(el) {
+  el.removeAttribute("hidden");
 }
-
+hideTosError(tosError);
+hideTosError(complaintSuccessfull);
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
+  if (tosCheckbox.checked) {
+    console.log("checkbox is checked");
+    hideTosError(tosError);
+
+    showTosError(complaintSuccessfull);
+  } else {
+    console.log("checkbox is not checked");
+    hideTosError(complaintSuccessfull);
+    showTosError(tosError);
+  }
 
   // --^-- write your code here --^--
 
