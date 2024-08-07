559a3ceea86e8c0a11912693417d4b222b61e22d
AntoineFachez <anthony.zornig@gmx.de>
2024-08-07 16:46:34 +0200

    refactor if to switch

--- js-forms-new/js-forms_calculator/js/index.js
+++ js-forms-new/js-forms_calculator/js/index.js
@@ -4,29 +4,37 @@ const form = document.querySelector('[data-js="form"]');
 const resultOutput = document.querySelector('[data-js="result"]');
 const allInputs = document.querySelectorAll("input");
 
-window.addEventListener("load", (e) => {
-  e.preventDefault();
-  selectedMethod();
-});
+// window.addEventListener("load", (e) => {
+//   e.preventDefault();
+//   selectedMethod();
+// });
 
 const selectedMethod = (res) => {
   let valueA, valueB;
+
   allInputs.forEach((element) => {
     if (element.id === "number-a") {
       valueA = +element.value;
     } else if (element.id === "number-b") {
       valueB = +element.value;
     }
-    if (element.value === "addition" && element.checked) {
-      res = +add(valueA, valueB);
-    } else if (element.value === "subtraction" && element.checked) {
-      res = +subtract(valueA, valueB);
-    } else if (element.value === "multiplication" && element.checked) {
-      res = +multiply(valueA, valueB);
-    } else if (element.value === "division" && element.checked) {
-      res = +divide(valueA, valueB);
-    }
   });
+
+  switch (true) {
+    case allInputs.find((el) => el.value === "addition" && el.checked):
+      res = +add(valueA, valueB);
+      break;
+    case allInputs.find((el) => el.value === "subtraction" && el.checked):
+      res = +subtract(valueA, valueB);
+      break;
+    case allInputs.find((el) => el.value === "multiplication" && el.checked):
+      res = +multiply(valueA, valueB);
+      break;
+    case allInputs.find((el) => el.value === "division" && el.checked):
+      res = +divide(valueA, valueB);
+      break;
+  }
+
   return res;
 };
 
