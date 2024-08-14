792d948980284c3f97eb549e90bfb07b2e750633
AntoineFachez <anthony.zornig@gmx.de>
2024-08-14 17:27:27 +0200

    Deploy Satellites solved

--- js-structure/js-structure_spacerocket/js/controls.js
+++ js-structure/js-structure_spacerocket/js/controls.js
@@ -1,11 +1,12 @@
-import { launch } from "./core/protocol.js";
-import * as launchSequence from "./launchSequence.js";
+import { launch } from './core/protocol.js';
+import * as launchSequence from './launchSequence.js';
 
 const launchSequenceFunction = launchSequence?.default;
 
 export function initialize() {
+  console.log(launchSequenceFunction());
   const launchButton = document.querySelector('[data-js="launch"]');
-  launchButton.addEventListener("click", () => {
+  launchButton.addEventListener('click', () => {
     launch(launchSequenceFunction);
   });
 }
