225dc35f26acc5a980305e006f6b19e6be96495d
AntoineFachez <anthony.zornig@gmx.de>
2024-08-20 11:13:41 +0200

    solve get-animal

--- js-unit-testing/js-unit-testing_get-animal/index.js
+++ js-unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,12 @@
-export function getAnimal() {}
+export function getAnimal(argument) {
+  if (argument === undefined) {
+    return "I do not like animals at all!";
+  } else if (typeof argument === "string") {
+    return `'I like ${argument}!'`;
+  } else if (argument === "cats") {
+    return "I totally love cats!";
+  }
+}
+console.log(getAnimal());
+console.log(getAnimal("dogs"));
+console.log(getAnimal("cats"));
