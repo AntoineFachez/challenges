23d6e49598da7efa8ac88bb1e2b0fe6cc8a53da7
AntoineFachez <anthony.zornig@gmx.de>
2024-08-23 15:27:24 +0200

    first commit

---
+++ typescript/src/index.ts
@@ -0,0 +1,49 @@
+export const init = () => {
+  console.log('init');
+};
+init();
+
+// const data = fetch(
+//   'https://66c862f28a477f50dc2dd4fa.mockapi.io/users/1/tasks',
+//   {
+//     method: 'GET',
+//     headers: { 'content-type': 'application/json' },
+//   }
+// )
+//   .then((res) => {
+//     if (res.ok) {
+//       return res.json();
+//     }
+//     // handle error
+//   })
+//   .then((tasks) => {
+//     // Do something with the list of tasks
+//   })
+//   .catch((error) => {
+//     // handle error
+//   });
+// console.log(data);
+
+// interface Contact {
+//   // TODO: Define the Contact interface
+//   id: number;
+//   name: string;
+//   email: string;
+//   phone: string;
+// }
+
+// let addressBook: Contact[] = [];
+
+// function addContact(contact: Contact): void {
+//   // TODO: Implement the addContact function
+// }
+
+// function removeContact(id: number): void {
+//   // TODO: Implement the removeContact function
+// }
+
+// function searchContacts(query: string): Contact[] {
+//   // TODO: Implement the searchContacts function
+//   const result = [];
+//   return result;
+// }
