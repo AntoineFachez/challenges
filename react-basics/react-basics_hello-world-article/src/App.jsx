83c0f7b7f5cb07a28bdff03eca5d56003dd9541c
AntoineFachez <anthony.zornig@gmx.de>
2024-08-23 15:47:48 +0200

    solve challenge

--- react-basics/react-basics_hello-world-article/src/App.jsx
+++ react-basics/react-basics_hello-world-article/src/App.jsx
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return HelloWorldArticle();
 }
+const HelloWorldArticle = () => (
+  <article>
+    <h1>Welcome back to the React Universe!</h1>
+    <p style={{ color: "white" }}>
+      Have a look at the [App.jsx](./src/App.jsx) file: the default export
+      function called `App` returns a div which wants to be replaced. Let us
+      comply with its request!
+    </p>
+  </article>
+);
