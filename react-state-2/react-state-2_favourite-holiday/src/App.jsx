48b42494c35dd4ca73138f558f45b7e95f210d1f
AntoineFachez <anthony.zornig@gmx.de>
2024-08-28 16:05:49 +0200

    solve challenge

--- react-state-2/react-state-2_favourite-holiday/src/App.jsx
+++ react-state-2/react-state-2_favourite-holiday/src/App.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 
 export default function App() {
-  function handleSubmit(event) {
-    event.preventDefault();
-  }
+  const [holiday, setHoliday] = useState("");
+  const [date, setDate] = useState(new Date().toLocaleDateString());
+  const [data, setData] = useState({ holiday: holiday, date: date });
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target); // Create a FormData object
+
+    const holiday = formData.get("holiday");
+    const date = formData.get("date");
+
+    setHoliday(holiday);
+    setDate(date);
+    handleResetValues();
+  }
+  // console.log(data.entries, data.entries());
+  const handleResetValues = () => {
+    setData({ holiday: holiday, date: date });
+    setHoliday("");
+    setDate("");
+  };
   return (
     <div className="container">
       <h1>Favourite Holiday Data Storage</h1>
@@ -19,20 +37,32 @@ export default function App() {
           id="holiday"
           type="text"
           name="holiday"
-          placeholder="e.g. Christmas"
+          placeholder="e.g. Women's World Day"
+          onChange={(e) => setHoliday(e.target.value)}
+          value={holiday}
         />
         <label htmlFor="date">Date: </label>
-        <input id="date" type="date" name="date" />
-        <button type="submit" className="form__submit-button">
+        <input
+          id="date"
+          type="date"
+          name="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="form__submit-button"
+          // onClick={(e) => handleSubmit(e, holiday, date)}
+        >
           Submit
         </button>
       </form>
       <h2>Output of Submitted Data</h2>
       <p>
-        Favourite Holiday: <span className="output">New Year</span>
+        Favourite Holiday: <span className="output">{data.holiday}</span>
       </p>
       <p>
-        Date: <span className="output">Well...</span>
+        Date: <span className="output">{data.date}</span>
       </p>
     </div>
   );
