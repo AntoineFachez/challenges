b4696d498f016f77c8237928c891fb607315dd68
AntoineFachez <anthony.zornig@gmx.de>
2024-08-29 11:21:34 +0200

    solve challange

--- react-state-3/react-state-3_tags/src/App.jsx
+++ react-state-3/react-state-3_tags/src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from "react";
-import Form from "./components/Form";
-import List from "./components/List";
-import "./App.css";
+import { useState } from 'react';
+import Form from './components/Form';
+import List from './components/List';
+import './App.css';
 
 export default function App() {
-  const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
+  const [tags, setTags] = useState(['JavaScript', 'React', 'CSS', 'HTML']);
+
+  const handleAddTag = (newTag) => {
+    console.log(newTag);
+    const newTagArray = [...tags, newTag];
+    setTags(newTagArray);
+  };
+  const handleDeleteTag = (tagToDelete) => {
+    console.log();
+    const newArray = tags.filter((tag) => tag !== tagToDelete);
+
+    setTags(newArray);
+  };
 
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={handleAddTag} />
+      <List tags={tags} onDeleteTag={handleDeleteTag} />
     </main>
   );
 }
