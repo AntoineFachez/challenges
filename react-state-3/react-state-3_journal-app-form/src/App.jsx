052e0121c6c2d9098a1b0751eac92c9071934de7
AntoineFachez <anthony.zornig@gmx.de>
2024-08-29 11:50:04 +0200

    solve challange

--- react-state-3/react-state-3_journal-app-form/src/App.jsx
+++ react-state-3/react-state-3_journal-app-form/src/App.jsx
@@ -1,16 +1,29 @@
-import EntriesSection from "./components/EntriesSection";
-import EntryForm from "./components/EntryForm";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import "./App.css";
+import EntriesSection from './components/EntriesSection';
+import EntryForm from './components/EntryForm';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import './App.css';
+import { initialEntries } from './assets/mockData';
+import { useState } from 'react';
+import { uid } from 'uid';
 
 function App() {
+  const [entries, setEntries] = useState(initialEntries);
+
+  const handleAddEntry = (newEntry) => {
+    const date = new Date().toLocaleDateString('en-us', {
+      dateStyle: 'medium',
+    });
+    const newArray = [{ ...newEntry, id: uid(), date: date }, ...entries];
+
+    setEntries(newArray);
+  };
   return (
     <div className="app">
       <Header />
       <main className="app__main">
-        <EntryForm />
-        <EntriesSection />
+        <EntryForm onAddEntry={handleAddEntry} />
+        <EntriesSection entries={entries} />
       </main>
       <Footer />
     </div>
--- react-state-3/react-state-3_journal-app-form/src/components/EntryForm/index.jsx
+++ react-state-3/react-state-3_journal-app-form/src/components/EntryForm/index.jsx
@@ -1,9 +1,21 @@
-import "./EntryForm.css";
-import Button from "../Button";
+import './EntryForm.css';
+import Button from '../Button';
 
-export default function EntryForm() {
+export default function EntryForm({ onAddEntry }) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+
+    const formData = {};
+    for (const [key, value] of data.entries()) {
+      formData[key] = value;
+    }
+
+    onAddEntry(formData);
+    e.target.reset();
+  };
   return (
-    <form className="entry-form">
+    <form className="entry-form" onSubmit={handleSubmit}>
       <h2 className="entry-form__title">New Entry</h2>
       <div className="entry-form__fields">
         <div className="entry-form__field">
