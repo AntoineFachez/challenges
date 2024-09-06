43f71ecdfd9b2c0dbdde2d715ad906ee4169bf8e
AntoineFachez <anthony.zornig@gmx.de>
2024-09-06 18:33:06 +0200

    push challange

--- react-global-state/react-global-state_lights/pages/_app.js
+++ react-global-state/react-global-state_lights/pages/_app.js
@@ -2,7 +2,6 @@ import GlobalStyle from "../styles";
 import Layout from "../components/Layout";
 
 export default function App({ Component, pageProps }) {
-  const initialState = {};
   return (
     <Layout>
       <GlobalStyle />
