119d937c32db64920ecf4a15f081ecfda7c173fa
AntoineFachez <anthony.zornig@gmx.de>
2024-09-09 11:11:05 +0200

    solve challange

--- react-immutable-state/react-immutable-state_car-race/pages/index.js
+++ react-immutable-state/react-immutable-state_car-race/pages/index.js
@@ -4,7 +4,7 @@ import Layout from "../components/Layout";
 export default function HomePage() {
   return (
     <Layout>
-      <h1>Car Race with useImmer</h1>
+      <h1>Car Race with useImmer()</h1>
       <CarRace />
     </Layout>
   );
